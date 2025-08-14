65d396e45511ed7bb4bebdb2bc8afa60e6dfa89b
BlueT13 <alex23456@naver.com>
2025-08-14 15:27:13 +0900

    [Fix] Vercel 배포 에러 수정

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,15 @@
 import { LoginForm } from "@/components/login-form";
 import HomeButton from "@/components/home-button";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <HomeButton variant="floating" />
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
